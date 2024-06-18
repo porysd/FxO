0590c1e365e3d79066f094b926f7ee3630605a43
Pot <potdiolola14@gmail.com>
2024-06-19 06:14:11 +0800

    Profile

--- app/src/main/java/com/example/fxo/PROFILE.java
+++ app/src/main/java/com/example/fxo/PROFILE.java
@@ -7,10 +7,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class PROFILE extends Fragment {
 
     ImageButton backBtn;
+    TextView fullname, username, name, contact, birthdate, settings, hs, logout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,14 +25,28 @@ public class PROFILE extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_p_r_o_f_i_l_e, container, false);
         backBtn = view.findViewById(R.id.back_btn);
+        fullname = view.findViewById(R.id.pfFULLNAME);
+        username = view.findViewById(R.id.pfUSERNAME);
+        name = view.findViewById(R.id.pfName);
+        contact = view.findViewById(R.id.pfContact);
+        birthdate = view.findViewById(R.id.pfBirthdate);
+
+        User user = User.getInstance();
+
+        String first = user.getFirstName();
+        String last = user.getLastName();
+
+        fullname.setText(first + " " + last);
+        username.setText("@" + user.getUserName());
+        name.setText("NAME: " + first + " " + last);
+        contact.setText("CONTACT: " + user.getContactNo());
+        birthdate.setText("BIRTHDATE: " + user.getBirthDate());
 
         backBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent (getActivity(), Nav.class);
-
+                Intent i = new Intent(getActivity(), Nav.class);
                 startActivity(i);
-
             }
         });
 
